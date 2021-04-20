4d2912f67202ae431c6be8b242399a53bb1cab30
wtwerner <wtwerner@gmail.com>
2021-04-20 09:07:01 -0500

    Configure auth0 provider

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <Router history={ history }>
-      <App />
-    </Router>
-  </Provider>,
+  <Auth0Provider
+    domain="wtwerner.us.auth0.com"
+    clientId="yH9P1WTLqjkIGe0BpF0EInAjQYdFV3In"
+    redirectUri={window.location.origin}
+  >
+    <Provider store={ store }>
+      <Router history={ history }>
+        <App />
+      </Router>
+    </Provider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
