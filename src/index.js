b7d5a2362b7824f37e4e6cc838476cd3365fa58e
wtwerner <wtwerner@gmail.com>
2021-04-20 13:48:48 -0500

    Remove auth0 provider

--- src/index.js
+++ src/index.js
@@ -6,22 +6,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="wtwerner.us.auth0.com"
-    clientId="yH9P1WTLqjkIGe0BpF0EInAjQYdFV3In"
-    redirectUri={window.location.origin}
-  >
-    <Provider store={ store }>
-      <Router history={ history }>
-        <App />
-      </Router>
-    </Provider>
-  </Auth0Provider>,
+  <Provider store={ store }>
+    <Router history={ history }>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
